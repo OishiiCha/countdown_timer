398c77770ded446e2d8796fba628de1d02a19064
OishiiCha <86476845+OishiiCha@users.noreply.github.com>
2021-09-21 13:41:06 +0100

    Linux install

---
+++ desktop_file.py
@@ -0,0 +1,13 @@
+import getpass
+import os
+
+f = open("countdown_2w.desktop", "x")
+
+usr = str(getpass.getuser())
+f.write("[Desktop Entry]\nVersion=1.0\nName=Countdown 2w\nComment=Simple countdown timer\nExec=python3 /home/"+ usr +"/countdow_timer/countdown2windows.py\nIcon=/home/"+ usr +"/countdown_timer/countdown.ico\nTerminal=false\nType=Application\nCategories=Application;")
+
+
+os.system("mv countdown_2w.desktop ~/Desktop/")
+os.system("cd")
+os.system("chmod +x ~/Desktop/countdown_2w.desktop")
+os.system("gio set ~/Desktop/countdown_2w.desktop metadata::trusted true")
