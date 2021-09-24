c65711695db71840b02e530de28211375d823390
OishiiCha <86476845+OishiiCha@users.noreply.github.com>
2021-09-24 12:02:23 +0100

    Update desktop_file_2w.py

--- desktop_file_2w.py
+++ desktop_file_2w.py
@@ -2,11 +2,13 @@ import getpass
 import os
 
 f = open("countdown_2w.desktop", "x")
+s = open("countdown2.sh", "x")
 
 usr = str(getpass.getuser())
-f.write("[Desktop Entry]\nVersion=1.0\nName=Countdown 2w\nComment=Simple countdown timer\nExec=python3 /home/"+ usr +"/countdown_timer/countdown2windows.py\nIcon=/home/"+ usr +"/countdown_timer/data/countdown.ico\nTerminal=false\nType=Application\nCategories=Application;")
+f.write("[Desktop Entry]\nVersion=1.0\nName=Countdown 2w\nComment=Simple countdown timer\nExec=python3 /home/"+ usr +"/countdown_timer/countdown2.sh\nIcon=/home/"+ usr +"/countdown_timer/data/countdown.ico\nTerminal=false\nType=Application\nCategories=Application;")
+s.write("#!/bin/bash\ncd countdown_timer\npython3 countdown2windows.py")
 
-
+os.system("chmod +x countdown2.sh")
 os.system("mv countdown_2w.desktop ~/Desktop/")
 os.system("cd")
 os.system("chmod +x ~/Desktop/countdown_2w.desktop")
